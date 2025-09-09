ecaaaa1e2bf2d456049d170246fe52c9db205303
Samkit-jain11 <jainsamkit1105@gmail.com>
2025-09-09 10:13:56 +0530

    Learning loop

---
+++ 15_practisequestion.c
@@ -0,0 +1,13 @@
+// printing number from 1 to 10 
+
+#include<stdio.h>
+
+int main() {
+    int i;
+    printf("I am going to print numbbers from 1 to 10 using loop \n ");
+
+    for (i=1; i<=10 ; i++) {
+        printf("%d \n", i);
+    }
+    return 0;
+}
