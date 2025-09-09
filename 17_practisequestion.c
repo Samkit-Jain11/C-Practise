295a23eb0aa7947a6cc7738794141872f6ffdb71
Samkit-jain11 <jainsamkit1105@gmail.com>
2025-09-09 11:10:04 +0530

    Next question for sum

---
+++ 17_practisequestion.c
@@ -0,0 +1,17 @@
+// take a number from user let us say n and then add it rom 1 , 2 .... till n
+
+#include<stdio.h>
+
+int main() {
+
+    int sum = 0 , n , i ;
+    printf("Enter the number till where you want to see the sum  :\n");
+    scanf("%d", &n);
+
+    for(i=0; i<=n; i++){
+    sum = sum + i;
+    }
+    printf("The sum of number entered from 1 to %d is %d \n", n , sum );
+
+    return 0;
+}
